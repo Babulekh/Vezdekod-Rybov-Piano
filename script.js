c4fdfa152982ef033879393bdfbc6f364f0140c4
vano23201 <vanca624@gmail.com>
2021-10-23 19:41:43 +0300

    20 points version

--- script.js
+++ script.js
@@ -1,64 +1,53 @@
 const whiteKeyCodes = ["KeyQ", "KeyW", "KeyE", "KeyR", "KeyT", "KeyY", "KeyU", "KeyI", "KeyO", "KeyP", "BracketLeft", "BracketRight"];
 const blackKeyCodes = ["Digit2", "Digit3", "Digit5", "Digit6", "Digit7", "Digit9", "Digit0", "Equal"];
 const whiteKeyNotes = ["Piano.ff.C3", "Piano.ff.D3", "Piano.ff.E3", "Piano.ff.F3", "Piano.ff.G3", "Piano.ff.A3", "Piano.ff.B3", "Piano.ff.C4", "Piano.ff.D4", "Piano.ff.E4", "Piano.ff.F4", "Piano.ff.G4"];
-const blackKeyNotes = ["Piano.ff.Db3", "Piano.ff.Eb3", "Piano.ff.Gb3", "Piano.ff.Ab3", "Piano.ff.Bb3", "Piano.ff.Db3", "Piano.ff.Eb3", "Piano.ff.Gb3"];
+const blackKeyNotes = ["Piano.ff.Db3", "Piano.ff.Eb3", "Piano.ff.Gb3", "Piano.ff.Ab3", "Piano.ff.Bb3", "Piano.ff.Db4", "Piano.ff.Eb4", "Piano.ff.Gb4"];
 
 const whiteKeys = document.querySelectorAll(".whiteKey");
 const blackKeys = document.querySelectorAll(".blackKey");
 
+const pianoKeys = [];
+
 class pianoKey {
-    constructor(code, sound, category) {
+    constructor(DOMObject, code, sound, category) {
+        this.DOMObject = DOMObject;
         this.code = code;
-        this.code = sound;
-        this.category = category;
-    }
-}
+        this.sound = new Audio("./sounds/" + sound + ".mp3");
 
-function keyboardKeyPressed(event) {
-    if (whiteKeyCodes.indexOf(event.code) != -1) {
-        whiteKeys[whiteKeyCodes.indexOf(event.code)].classList.add("active");
-        let sound = new Audio("./sounds/" + whiteKeyNotes[whiteKeyCodes.indexOf(event.code)] +".mp3");
-        sound.addEventListener("ended", () => console.log("ended"));
-        sound.play();
-    }
+        this.DOMObject.addEventListener("mousedown", this.pianoKeyPressed.bind(this));
+        this.DOMObject.addEventListener("mouseup", this.pianoKeyReleased.bind(this));
+    };
+};
 
-    if (blackKeyCodes.indexOf(event.code) != -1) {
-        blackKeys[blackKeyCodes.indexOf(event.code)].classList.add("active");
-        let sound = new Audio("./sounds/" + blackKeyNotes[blackKeyCodes.indexOf(event.code)] +".mp3");
-        sound.addEventListener("ended", () => console.log("ended"));
-        sound.play();
-    }
-}
+pianoKey.prototype.pianoKeyPressed = function () {
+    this.DOMObject.classList.add("active");
+    this.sound.currentTime = 0;
+    this.sound.play();
+};
 
-function keyboardKeyReleased(event) {
-    if (whiteKeyCodes.indexOf(event.code) != -1) {
-        whiteKeys[whiteKeyCodes.indexOf(event.code)].classList.remove("active");
-    }
-
-    if (blackKeyCodes.indexOf(event.code) != -1) {
-        blackKeys[blackKeyCodes.indexOf(event.code)].classList.remove("active");
-    }
-}
-
-function keyPressed(event) {
-    event.currentTarget.classList.add("active");
-    //Проигрывать звук, Убирать класс когда кончается звук клавиши
-}
-
-function keyReleased(event) {
-    event.currentTarget.classList.remove("active");
-    //Прекращать играть звук клавиши
-}
+pianoKey.prototype.pianoKeyReleased = function () {
+    this.DOMObject.classList.remove("active");
+    // this.sound.pause();
+};
 
 for (let wki = 0; wki < whiteKeys.length; wki++) {
-    whiteKeys[wki].addEventListener("mousedown", keyPressed);
-    whiteKeys[wki].addEventListener("mouseup", keyReleased);
-}
+    let key = new pianoKey(whiteKeys[wki], whiteKeyCodes[wki], whiteKeyNotes[wki]);
+    pianoKeys.push(key);
+};
 
 for (let bki = 0; bki < blackKeys.length; bki++) {
-    blackKeys[bki].addEventListener("mousedown", keyPressed);
-    blackKeys[bki].addEventListener("mouseup", keyReleased);
-}
+    let key = new pianoKey(blackKeys[bki], blackKeyCodes[bki], blackKeyNotes[bki]);
+    pianoKeys.push(key);
+};
+
+function keyboardKeyPressed(event) {
+    if (event.repeat) return;
+    pianoKeys.find(element => {return event.code == element.code;})?.pianoKeyPressed();
+};
+
+function keyboardKeyReleased(event) {
+    pianoKeys.find(element => {return event.code == element.code;})?.pianoKeyReleased();
+};
 
 window.addEventListener("keydown", keyboardKeyPressed);
 window.addEventListener("keyup", keyboardKeyReleased);
