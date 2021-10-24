6da67bf1466bc13ef0238685e514bad3516a0742
vano23201 <vanca624@gmail.com>
2021-10-24 17:41:09 +0300

    Version for 40 points

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 //Data
 const whiteKeyCodes = ["Tab", "KeyQ", "KeyW", "KeyE", "KeyR", "KeyT", "KeyY", "KeyU", "KeyI", "KeyO", "KeyP", "BracketLeft", "BracketRight", "Enter", "Delete"];
 const blackKeyCodes = ["Digit2", "Digit3", "Digit5", "Digit6", "Digit7", "Digit9", "Digit0", "Equal", "Backspace", "Insert"];
-const whiteKeyNotes = ["B3", "C3", "D3", "E3", "F3", "G3", "A3", "B3", "C4", "D4", "E4", "F4", "G4", "A4", "B4"];
+const whiteKeyNotes = ["B2", "C3", "D3", "E3", "F3", "G3", "A3", "B3", "C4", "D4", "E4", "F4", "G4", "A4", "B4"];
 const blackKeyNotes = ["Db3", "Eb3", "Gb3", "Ab3", "Bb3", "Db4", "Eb4", "Gb4", "Ab4", "Bb4"];
 let melodySrc = "melody.xml";
 let xmldoc;
-
+parseXML();
 //Ui
 const whiteKeys = document.querySelectorAll(".whiteKey");
 const blackKeys = document.querySelectorAll(".blackKey");
@@ -17,9 +17,10 @@ const pianoRollButton = document.querySelector(".pianoRollButton");
 class Piano {
     constructor(whiteKeyCodes, blackKeyCodes, whiteKeyNotes, blackKeyNotes) {
         this.pianoKeys = [];
-        this.currentIndex = 0;
+        this.currentNote = 0;
         this.timer;
         this.noteDuration;
+        this.melody = [];
 
         for (let wki = 0; wki < whiteKeys.length; wki++) {
             let key = new pianoKey(whiteKeys[wki], whiteKeyCodes[wki], whiteKeyNotes[wki]);
@@ -36,7 +37,7 @@ class Piano {
 Piano.prototype.startPlayback = function () {
     let sequence = xmldoc.querySelectorAll("measure");
     this.noteDuration = 60000 / Number(xmldoc.querySelector("per-minute").innerHTML);
-    let melody = [];
+    this.melody = [];
 
     for (let beat of sequence) {
         let notes = beat.querySelectorAll("note pitch");
@@ -51,61 +52,105 @@ Piano.prototype.startPlayback = function () {
             if (duration < 100) {
                 duration *= 10;
             }
-            melody.push({ keyIndex: this.pianoKeys.indexOf(key), duration: (duration / 240) * this.noteDuration });
+            this.melody.push({ keyIndex: this.pianoKeys.indexOf(key), duration: (duration / 240) * this.noteDuration });
         }
-        melody.push({ keyIndex: -1, duration: 600 });
     }
 
     this.timer = setInterval(() => {
-        this.playNote(melody);
+        this.playNote();
     }, this.noteDuration);
 };
 
-Piano.prototype.pianoRoll = function () {
-    return 0;
+Piano.prototype.startPianoRoll = function () {
+    let sequence = xmldoc.querySelectorAll("measure");
+    this.noteDuration = 60000 / Number(xmldoc.querySelector("per-minute").innerHTML);
+    this.melody = [];
 };
 
-Piano.prototype.playNote = function (melody) {
-    if (melody[this.currentIndex]["keyIndex"] == -1) {
-        this.currentIndex++;
-    }
-
-    if (this.currentIndex == melody.length) {
+Piano.prototype.playNote = function () {
+    if (this.currentNote == this.melody.length) {
         this.currentIndex = 0;
         clearInterval(this.timer);
         playBackButton.removeAttribute("disabled");
         return;
     }
 
-    let key = this.pianoKeys[melody[this.currentIndex]["keyIndex"]];
-    let duration = melody[this.currentIndex]["duration"];
+    let key = this.pianoKeys[this.melody[this.currentNote]["keyIndex"]];
+    let duration = this.melody[this.currentNote]["duration"];
 
     key.pianoKeyPressed(duration);
-    setTimeout(key.pianoKeyReleased.bind(key), melody[this.currentIndex]["duration"]);
+    setTimeout(key.pianoKeyReleased.bind(key), this.melody[this.currentNote]["duration"]);
 
-    this.currentIndex++;
+    this.currentNote++;
 };
 
 class pianoKey {
     constructor(DOMObject, code, sound) {
         this.DOMObject = DOMObject;
         this.code = code;
-        this.sound = new Audio("./sounds/Piano.ff." + sound + ".mp3");
         this.note = sound;
+        this.sound = playKey.bind(this, this.getPitch());
 
-        this.DOMObject.addEventListener("mousedown", this.pianoKeyPressed.bind(this, -1));
-        this.DOMObject.addEventListener("mouseup", this.pianoKeyReleased.bind(this, -1));
+        this.DOMObject.addEventListener("mousedown", this.pianoKeyPressed.bind(this, 1));
+        this.DOMObject.addEventListener("mouseup", this.pianoKeyReleased.bind(this, 1));
     }
 }
 
-pianoKey.prototype.pianoKeyPressed = function (duration = -1) {
-    //if (this.DOMObject.classList.contains("active")) return;
-    //150 - 600
+pianoKey.prototype.getPitch = function () {
+    let octave = this.note.charCodeAt(this.note.length - 1) - 48;
+    let shift = this.note.charAt(0);
+    let b = this.note.charCodeAt(1) == 98;
+    shift += b ? "b" : "";
+    let pitch;
+
+    switch (shift) {
+        case "C":
+            pitch = 0;
+            break;
+        case "Db":
+            pitch = 1;
+            break;
+        case "D":
+            pitch = 2;
+            break;
+        case "Eb":
+            pitch = 3;
+            break;
+        case "E":
+            pitch = 4;
+            break;
+        case "F":
+            pitch = 5;
+            break;
+        case "Gb":
+            pitch = 6;
+            break;
+        case "G":
+            pitch = 7;
+            break;
+        case "Ab":
+            pitch = 8;
+            break;
+        case "A":
+            pitch = 9;
+            break;
+        case "Bb":
+            pitch = 10;
+            break;
+        case "B":
+            pitch = 11;
+            break;
+    }
+
+    pitch = octave * 12 + pitch;
+    return pitch;
+};
+
+pianoKey.prototype.pianoKeyPressed = function (duration = 1.0) {
+    if (this.DOMObject.classList.contains("active")) return;
+    if (duration != 1) duration /= 1000;
     this.DOMObject.classList.add("active");
-    this.sound.playbackRate = 1;
-    if (duration != -1) this.sound.playbackRate = (this.sound.duration * 1000) / duration;
-    this.sound.currentTime = 0;
-    this.sound.play();
+    this.sound(duration);
 };
 
 pianoKey.prototype.pianoKeyReleased = function () {
@@ -136,23 +181,21 @@ window.addEventListener("keydown", keyboardKeyPressed);
 window.addEventListener("keyup", keyboardKeyReleased);
 
 function parseXML(event) {
-    event.currentTarget.setAttribute("disabled", "");
+    event?.currentTarget.setAttribute("disabled", "");
 
     let xhr = new XMLHttpRequest();
     xhr.open("GET", melodySrc);
     xhr.send();
+    let parser = new DOMParser();
 
-    xhr.onload = () => {
-        let parser = new DOMParser();
+    xhr.addEventListener("load", () => {
         xmldoc = parser.parseFromString(xhr.responseText, "text/xml");
-        // if (event.currentTarget.classList[0] == "playBackButton") {
-        //     piano.startPlayback();
-        // } else {
-        //     piano.pianoRoll();
-        // };
-        piano.startPlayback();
-    };
+    });
 }
 
-playBackButton?.addEventListener("click", parseXML);
-pianoRollButton?.addEventListener("click", parseXML);
+playBackButton?.addEventListener("click", () => {
+    piano.startPlayback();
+});
+pianoRollButton?.addEventListener("click", () => {
+    piano.startPianoRoll();
+});
